8c8be1b71b51e5c20a570b23ec8de607338563f6
zanzuri1234 <125349200+zanzuri1234@users.noreply.github.com>
2023-02-14 09:22:05 +0100

    Add files via upload

---
+++ gyuftyj.c
@@ -0,0 +1,13 @@
+#include <stadio.h>
+
+
+int main()
+{
+   char ciao;
+   printf("scrivi uns lettera")
+   scanf("%c",&ciao);
+   ciao= ciao+32;
+   printf("ecco la lettera in maiuscolo %c", ciao);
+
+}
+
